ef7956544803135904250ca736bf8da8cb6c3109
lijiang <1406792294@qq.com>
2018-08-02 09:56:55 +0800

    Feature: url开发
    1.添加测试代码

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,10 @@
 import { Url } from "../net/Url";
 
-
+/**
+ * Url测试代码
+ */
 const ip = "http://wwww.baidu.com";
 const route = "/tool/";
 const params = {date: "2018-08-01 +800", time: "20:26"};
 const url = new Url(ip, route, params);
-
 console.log(url.toString());
